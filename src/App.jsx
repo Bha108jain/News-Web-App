52e04db852371556a31ad3f4537146933a0d53ee
Bha108jain <bhavyajainn1008@gmail.com>
2024-07-16 19:41:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import NewsBoard from './Components/NewsBoard'
+import {useState} from 'react'
+export const App = () => {
+   const[category,setCategory]=useState("general");
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <NewsBoard category={category}/>
+    </div>
+  )
+}
+export default App
